0e213ab4be4f82acca464679bcde6d83c80ecdf7
fparadas <felipegparadas@gmail.com>
2023-09-24 18:24:27 -0300

    day 1 and 2 of aoc in rust

---
+++ src/day_one.rs
@@ -0,0 +1,41 @@
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut buff = String::new();
+
+    for line in stdin.lock().lines() {
+        buff.push_str(&line?);
+        buff.push_str("\n");
+    }
+    let elves = parse(&buff);
+    let top = find_max(&elves);
+
+    println!("Top Calories: {:#?}", top);
+    println!("Top Three: {:#?}", find_top_three(&elves));
+    Ok(())
+}
+
+fn parse(input: &str) -> Vec<i32> {
+    input
+        .split("\n\n")
+        .map(|x| {
+            x.split("\n")
+                .filter(|x| !x.is_empty())
+                .map(|x| x.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect()
+}
+
+fn find_max(input: &Vec<i32>) -> i32 {
+    input.iter().max().unwrap().clone()
+}
+
+fn find_top_three(input: &Vec<i32>) -> i32 {
+    let mut arr = input.clone();
+    arr.sort();
+    arr.reverse();
+    arr.truncate(3);
+    arr.iter().sum::<i32>()
+}
---
+++ src/day_two.rs
@@ -0,0 +1,118 @@
+use std::io::{self, BufRead};
+
+enum GameInput {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn move_from_string(input: &str) -> GameInput {
+    match input {
+        "A" | "X" => GameInput::Rock,
+        "B" | "Y" => GameInput::Paper,
+        "C" | "Z" => GameInput::Scissors,
+        _ => panic!("Invalid input"),
+    }
+}
+
+fn shape_score(input: &GameInput) -> i32 {
+    match input {
+        GameInput::Rock => 1,
+        GameInput::Paper => 2,
+        GameInput::Scissors => 3,
+    }
+}
+
+fn game_score(adversary: &GameInput, player: &GameInput) -> i32 {
+    shape_score(player)
+        + match adversary {
+            GameInput::Rock => match player {
+                GameInput::Rock => 3,
+                GameInput::Paper => 6,
+                GameInput::Scissors => 0,
+            },
+            GameInput::Paper => match player {
+                GameInput::Rock => 0,
+                GameInput::Paper => 3,
+                GameInput::Scissors => 6,
+            },
+            GameInput::Scissors => match player {
+                GameInput::Rock => 6,
+                GameInput::Paper => 0,
+                GameInput::Scissors => 3,
+            },
+        }
+}
+
+enum Strategy {
+    Win,
+    Draw,
+    Lose,
+}
+
+fn strat_from_string(input: &str) -> Strategy {
+    match input {
+        "X" => Strategy::Lose,
+        "Y" => Strategy::Draw,
+        "Z" => Strategy::Win,
+        _ => panic!("Invalid input"),
+    }
+}
+
+fn follow_stat(adversary: &GameInput, player: &Strategy) -> i32 {
+    match adversary {
+        GameInput::Rock => match player {
+            Strategy::Win => game_score(adversary, &GameInput::Paper),
+            Strategy::Draw => game_score(adversary, &GameInput::Rock),
+            Strategy::Lose => game_score(adversary, &GameInput::Scissors),
+        },
+        GameInput::Paper => match player {
+            Strategy::Win => game_score(adversary, &GameInput::Scissors),
+            Strategy::Draw => game_score(adversary, &GameInput::Paper),
+            Strategy::Lose => game_score(adversary, &GameInput::Rock),
+        },
+        GameInput::Scissors => match player {
+            Strategy::Win => game_score(adversary, &GameInput::Rock),
+            Strategy::Draw => game_score(adversary, &GameInput::Scissors),
+            Strategy::Lose => game_score(adversary, &GameInput::Paper),
+        },
+    }
+}
+
+fn parse(input: &str) -> Vec<(GameInput, Strategy)> {
+    input
+        .split("\n")
+        .filter(|x| !x.is_empty())
+        .map(|x| {
+            let mut chars = x.chars();
+            let adversary = move_from_string(&chars.next().unwrap().to_string());
+            chars.next(); //skip whitespace
+            let player = strat_from_string(&chars.next().unwrap().to_string());
+            (adversary, player)
+        })
+        .collect()
+}
+
+fn read_lines() -> io::Result<String> {
+    let stdin = io::stdin();
+    let mut buff = String::new();
+
+    for line in stdin.lock().lines() {
+        buff.push_str(&line?);
+        buff.push_str("\n");
+    }
+
+    Ok(buff)
+}
+
+fn main() -> io::Result<()> {
+    let buff = read_lines()?;
+
+    let total_score = parse(&buff)
+        .iter()
+        .map(|x| follow_stat(&x.0, &x.1))
+        .sum::<i32>();
+
+    println!("Total Score: {:#?}", total_score);
+    Ok(())
+}
